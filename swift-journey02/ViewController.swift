2676ba02c13ccb5d3781a9621f8d2e66fc21e2ed
Leonardo Kameya <leonardo.kameya@gmail.com>
2018-08-08 16:47:04 -0400

    Initial Commit

---
+++ swift-journey02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-journey02
+//
+//  Created by Leonardo Kameya on 08/08/18.
+//  Copyright © 2018 Leonardo Kameya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
