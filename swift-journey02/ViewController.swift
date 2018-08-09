b488fc8bb2dab9b8804e3f54f63319e0a9cc12f1
Leonardo Kameya <leonardo.kameya@gmail.com>
2018-08-09 16:23:45 -0400

    Adding assets and changing images randomly

--- swift-journey02/ViewController.swift
+++ swift-journey02/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    var randomDiceIndex3 : Int = 0
+    var randomDiceIndex4 : Int = 0
+    var randomDiceIndex5 : Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView3: UIImageView!
+    @IBOutlet weak var diceImageView4: UIImageView!
+    @IBOutlet weak var diceImageView5: UIImageView!
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func goButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        randomDiceIndex3 = Int(arc4random_uniform(6))
+        randomDiceIndex4 = Int(arc4random_uniform(6))
+        randomDiceIndex5 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        diceImageView3.image = UIImage(named: diceArray[randomDiceIndex3])
+        diceImageView4.image = UIImage(named: diceArray[randomDiceIndex4])
+        diceImageView5.image = UIImage(named: diceArray[randomDiceIndex5])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
 }
 
